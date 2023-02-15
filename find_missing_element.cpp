9aa49c2716d549861b61eb75710fd24a956481e6
Srishti Saurav <103064640+SrishtiSaurav22@users.noreply.github.com>
2023-02-15 22:37:59 +0530

    easy

--- find_missing_element.cpp
+++ find_missing_element.cpp
@@ -2,34 +2,34 @@
 
 using namespace std;
 
-int find_missing_element(vector<int>& arr, int n)
+int findMissingElement(vector<int> vec, int n)
 {
-    int i;
+    int expectedSum=(n*(n+1))/2;
+    int sum=0;
 
-    for(i=1;i<=n;i++)
-    {
-        if(*find(arr.begin(),arr.end()+1,i)==0)
-            return i;
-    }
+    vector<int>::iterator itr;
 
-    return 0;
+    for(itr=vec.begin();itr!=vec.end();itr++)
+        sum=sum+*itr;
+
+    return expectedSum-sum;
 }
 
 int main()
 {
     vector<int> vec;
 
+    vec.push_back(6);
+    vec.push_back(1);
     vec.push_back(2);
+    vec.push_back(8);
     vec.push_back(3);
     vec.push_back(4);
-    vec.push_back(1);
-    vec.push_back(6);
     vec.push_back(7);
+    vec.push_back(10);
+    vec.push_back(5);
 
-    cout<<"Missing element: "<<find_missing_element(vec,7)<<endl;
-    // if result is zero there's no missing element, but that won't be the case
-
-    cout<<*find(vec.begin(),vec.end()+1,5); //return the iterator to that element
+    cout<<"Missing element: "<<findMissingElement(vec,10)<<endl;
 
     cout<<endl;
     return 0;
