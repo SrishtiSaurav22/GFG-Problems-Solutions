53e65131f5fae0736b3f213b88100b96fda3ddea
Srishti Saurav <103064640+SrishtiSaurav22@users.noreply.github.com>
2023-02-16 23:36:47 +0530

    easy, find better solution?

--- sort_0_1_2_array.cpp
+++ sort_0_1_2_array.cpp
@@ -1,3 +1,35 @@
+/*
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+EXAMPLE 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+EXAMPLE 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -11,6 +43,8 @@ void printVector(vector<int> vec)
 }
 
 // this function takes O(3n)
+// find solution that takes O(n) time?
+// the two main things are that: array should be sorted in place and that it should be done in O(n) time
 vector<int> sort012(vector<int> vec)
 {
     int i,n0=0,n1=0,n2=0;
